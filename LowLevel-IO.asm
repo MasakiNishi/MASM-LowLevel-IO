158c77db17aec7d711e71c8b98f40c61a4a36e14
MasakiNishi <n24mfs@gmail.com>
2023-08-17 04:43:24 +0900

    Add: LowLevel-IO program

---
+++ LowLevel-IO.asm
@@ -0,0 +1,22 @@
+TITLE String Primitives and Macros Parameters
+
+; Author:					Masaki Nishi
+; Description:
+
+INCLUDE Irvine32.inc
+
+.data
+
+; statements
+
+; variables
+
+.code
+
+main PROC
+
+	; exit to operating system
+	Invoke	ExitProcess,0
+main ENDP
+
+END main
